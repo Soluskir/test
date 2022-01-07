d8c2154b1806f1252c4d2b7ffca598da0ea3acaf
Soluskir <97241002+Soluskir@users.noreply.github.com>
2022-01-07 22:10:38 +0500

    Изменил персонажа, добавил Input System
    Убрал старого персонажа, т.к. анимации были выполнены технически неграмотно.
    Добавил нового, создал анимации в состоянии стоя, бега, стрельбы, прыжка.
    Добавил Input System.
    Появились механики бега, прыжка, стояния на месте.

--- New Unity Project/Assets/Scripts/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,80 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] float runSpeed = 5f;
+    [SerializeField] float jumpRange = 15f;
+
+    Vector2 moveInput;
+    Rigidbody2D rigidbody2D;
+    Animator myAnimator;
+    BoxCollider2D feetCollider;
+
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        feetCollider = GetComponent<BoxCollider2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+        Jumping();
+    }
+
+    void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, rigidbody2D.velocity.y);
+        rigidbody2D.velocity = playerVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", playerHasHorizontalSpeed);
+    }
+
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(rigidbody2D.velocity.x) > Mathf.Epsilon;
+
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rigidbody2D.velocity.x), 1f);
+        }
+    }
+
+    void OnJump(InputValue value)
+    {
+        if (!feetCollider.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+        {
+            return;
+        }
+
+        if (value.isPressed)
+        {
+            rigidbody2D.velocity += new Vector2(0f, jumpRange);
+            myAnimator.SetBool("isJumping", true);
+        }
+    }
+
+    void Jumping()
+    {
+        if (!feetCollider.IsTouchingLayers(LayerMask.GetMask("Platforms")))
+        {
+            myAnimator.SetBool("isJumping", true);
+        }
+        else
+        {
+            myAnimator.SetBool("isJumping", false);
+        }
     }
 }
