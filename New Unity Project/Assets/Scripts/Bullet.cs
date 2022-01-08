85a9392a7a3ed4661d68ec858c4dea0e74ebab23
Soluskir <97241002+Soluskir@users.noreply.github.com>
2022-01-08 13:53:23 +0500

    Add some objects

--- New Unity Project/Assets/Scripts/Bullet.cs
+++ New Unity Project/Assets/Scripts/Bullet.cs
@@ -6,22 +6,20 @@ public class Bullet : MonoBehaviour
 {
     [SerializeField] float bulletSpeed = 10f;
 
-    Rigidbody2D rigidbody2D;
+    Rigidbody2D rb;
     PlayerMovement player;
-    bool isShooting;
     float xSpeed;
 
     void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
         player = FindObjectOfType<PlayerMovement>();
         xSpeed = player.transform.localScale.x * bulletSpeed;
     }
 
     void Update()
     {
-        rigidbody2D.velocity = new Vector2(xSpeed, 0f);
-        isShooting = true;
+        rb.velocity = new Vector2(xSpeed, 0f);
     }
 
     void OnCollisionEnter2D(Collision2D collision)
